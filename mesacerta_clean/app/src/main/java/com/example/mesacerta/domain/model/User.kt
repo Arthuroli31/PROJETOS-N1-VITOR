d487c11a2a38145933a449ccf602aa87a520c74c
Arthuroli31 <arthuroliveira0600@gmail.com>
2025-10-06 10:16:19 -0300

    Delete mesacerta_clean directory

--- mesacerta_clean/app/src/main/java/com/example/mesacerta/domain/model/User.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mesacerta.domain.model
-
-data class User(
-    val id: Int,
-    val name: String,
-    val email: String,
-    val phone: String,
-    val profileImage: String? = null
-)
